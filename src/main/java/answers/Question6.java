b258fcbce28cb14982e04b002864a67bb2c6a814
Chuan Tan <ct538@cam.ac.uk>
2018-10-15 20:34:30 +0100

    wee2

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -1,10 +1,51 @@
 package answers;
 
-public class Question6 {
+import java.util.PriorityQueue;
 
+public class Question6 {
+	private static Node[] vertices;
+	public static void main(String[] args) {
+		System.out.println(shortestServerRoute(3,1, new int[][] { new int[] {0,7,4} , new int[] {7,0,2} , new int[] {4,2,0} } ));
+	}
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
 		// TODO Auto-generated method stub
-		return -1;
+		vertices = new Node[numServers];
+		for(int i =0;i<vertices.length;i++) {
+			vertices[i] = new Node();
+			vertices[i].index = i;
+			vertices[i].distance = Integer.MAX_VALUE;
+		}
+		vertices[0].distance = 0;
+		PriorityQueue<Node> toexplore = new PriorityQueue<Node>();
+		for(int i =0;i<vertices.length;i++) {
+			toexplore.add(vertices[i]);
+		}
+		
+		while(!toexplore.isEmpty()) {
+			Node v = toexplore.poll();
+			System.out.println("removed server: "+ v.index);
+			for(int i = 0;i<numServers;i++) {
+				if(v.index != i && times[v.index][i] >= 0) {
+					vertices[i].distance = Math.min(v.distance + times[v.index][i], vertices[i].distance);
+				}
+				
+			}
+		}
+		return vertices[targetServer].distance;
+		
 	}
 
+}
+
+class Node implements Comparable {
+	public int index;
+	public int distance;
+
+	@Override
+	public int compareTo(Object o) {
+		Node n2 = (Node) o;
+		if(distance < n2.distance) return -1;
+		if(distance > n2.distance) return 1;
+		return 0;
+	}
 }
