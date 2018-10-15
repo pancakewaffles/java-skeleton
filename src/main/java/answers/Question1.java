df923083aeee9cae4eb95045ebb9467d5e007003
Chuan Tan <ct538@cam.ac.uk>
2018-10-15 19:48:08 +0100

    wweeeee

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,22 @@
 package answers;
 
 public class Question1 {
-
+	private static int[][] answer_matrix;
+	private static int max = 0;
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		answer_matrix = new int[portfolios.length][portfolios.length];
+		for(int i =0;i<portfolios.length;i++) {
+			for(int j =0;j<portfolios.length;j++) {
+				if(i != j) {
+					answer_matrix[i][j] = portfolios[i] ^ portfolios[j];
+					max = Math.max(max, answer_matrix[i][j]);
+				}else {
+					answer_matrix[i][j] = 0;
+				}
+			}
+		}
+		return max;
 	}
+	
 
 }
--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,10 +1,36 @@
 package answers;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Question5 {
+	private static Set<Integer> values;
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
+		values = new HashSet<Integer>();
+		for(int i : allowedAllocations) {
+			values.add(i);
+		}
+		return f(totalValue);
 
+}
+
+	private static int f(int totalValue) {
+		if(values.contains(totalValue)) {
+			return 1;
+		}
+		if(totalValue == 0) {
+			return 0;
+		}
+		if(totalValue < 0) {
+			return Integer.MAX_VALUE;
+		}
+		int min = Integer.MAX_VALUE;
+		for(int i : values) {
+			min = Math.min(min, f(totalValue-i));
+		}
+		return 1 + min;
+	}
 }
